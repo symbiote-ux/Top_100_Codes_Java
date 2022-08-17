f54feeca75d7fb5a1ec775d6a6ff2e98fcb41818
Symbiote-ux <tnikhil422@gmail.com>
2022-08-17 16:16:48 +0530

    function to check strong num added

--- StrongNum.java
+++ StrongNum.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class StrongNum {
-  
+
+  static int factorial(int num) {
+    if (num == 0) {
+      return 1;
+    }
+    return num * factorial(num - 1);
+  }
+  static boolean isStrong(int num) {
+    int temp = num;
+    int sum = 0;
+    while (temp != 0) {
+      int digit = temp % 10;
+      int facto = factorial(digit);
+      sum += facto;
+      temp /= 10;
+    }
+    return sum == num;
+  }
+
+  public static void main(String args[]) {
+    Scanner s = new Scanner(System.in);
+    int num = s.nextInt();
+    if (isStrong(num)) {
+      System.out.println("Strong");
+    } else {
+      System.out.println("Not Strong");
+    }
+  }
 }
